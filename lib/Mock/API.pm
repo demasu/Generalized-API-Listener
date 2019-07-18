a2c4917204160343ef5bb3616244ee63d3ed9c02
Joshua Brandt <joshua.brandt@cpanel.net>
2019-07-18 17:41:46 -0500

    Removed debugging statements

--- lib/Mock/API.pm
+++ lib/Mock/API.pm
@@ -11,62 +11,39 @@ our $VERSION = '0.1';
 
 set 'show_errors' => 0;
 
-use Data::Dumper;
-$Data::Dumper::Indent = 3;
-
 get '/' => sub {
     template 'index' => { 'title' => 'Mock::API' };
 };
 
 get '/order' => sub {
-    print STDERR "# API.pm: Getting params\n";
     my $params = params;
-    print STDERR "# API.pm: Params are:\n";
-    print STDERR "# API.pm: API.pm: \n" . Dumper( \$params ) . "\n";
-    print STDERR "# API.pm: Calling handle_request on Mock::API::Order\n";
     my $result = Mock::API::Order::handle_request( $params );
     $result = JSON::MaybeXS::encode_json( $result );
 
-    print STDERR "# API.pm: Returning result\n";
     return $result;
 };
 
 get '/cancel' => sub {
-    print STDERR "# API.pm: Getting params\n";
     my $params = params;
-    print STDERR "# API.pm: Params are:\n";
-    print STDERR "# API.pm: API.pm: \n" . Dumper( \$params ) . "\n";
-    print STDERR "# API.pm: Calling handle_request on Mock::API::Cancel\n";
     my $result = Mock::API::Cancel::handle_request( $params );
     $result = JSON::MaybeXS::encode_json( $result );
 
-    print STDERR "# API.pm: Returning result\n";
     return $result;
 };
 
 post '/order' => sub {
-    print STDERR "# API.pm: Getting params\n";
     my $params = params;
-    print STDERR "# API.pm: Params are:\n";
-    print STDERR "# API.pm: API.pm: \n" . Dumper( \$params ) . "\n";
-    print STDERR "# API.pm: Calling handle_request on Mock::API::Order\n";
     my $result = Mock::API::Order::handle_request( $params );
     $result = JSON::MaybeXS::encode_json( $result );
 
-    print STDERR "# API.pm: Returning result\n";
     return $result;
 };
 
 post '/cancel' => sub {
-    print STDERR "# API.pm: Getting params\n";
     my $params = params;
-    print STDERR "# API.pm: Params are:\n";
-    print STDERR "# API.pm: API.pm: \n" . Dumper( \$params ) . "\n";
-    print STDERR "# API.pm: Calling handle_request on Mock::API::Cancel\n";
     my $result = Mock::API::Cancel::handle_request( $params );
     $result = JSON::MaybeXS::encode_json( $result );
 
-    print STDERR "# API.pm: Returning result\n";
     return $result;
 };
 
--- lib/Mock/API/Order.pm
+++ lib/Mock/API/Order.pm
@@ -3,12 +3,8 @@ package Mock::API::Order;
 use strict;
 use warnings;
 
-use Data::Dumper;
-$Data::Dumper::Indent = 3;
-
 sub handle_request {
     my ( $params ) = @_;
-    print STDERR "# Order.pm: In handle_request\n";
 
     my $response = {
         'result'  => 1,
@@ -17,16 +13,12 @@ sub handle_request {
     };
 
     my ($quantity_name) = grep { $_ =~ /quantity|amount|number/i } keys(%{$params});
-    print STDERR "# Order.pm: Quantity name is: [$quantity_name]\n";
     my $quantity = delete $params->{$quantity_name};
-    print STDERR "# Order.pm: Quantity is: [$quantity]\n";
 
     my %obj = ();
     foreach my $param ( keys( %{$params} ) ) {
         $obj{$param} = $params->{$param};
     }
-    print STDERR "# Order.pm: Object built is:\n";
-    print STDERR "# Order.pm: handle_request: \n" . Dumper( \%obj ) . "\n";
 
     my @licenses;
     my $i = 0;
@@ -34,22 +26,14 @@ sub handle_request {
         my @chars = ( "A".."Z", 1..9 );
         my $string = 'KEY-';
         $string   .= $chars[rand @chars] for 1..15;
-        print STDERR "# Order.pm: String is: [$string]\n";
         my %temp   = %obj;
-        print STDERR "# Order.pm: handle_request: \n" . Dumper( \%temp ) . "\n";
         $temp{license_key} = $string;
         push @licenses, \%temp;
         $i++;
         last if $i >= 10;
     }
-    print STDERR "# Order.pm: Licenses are:\n";
-    print STDERR "# Order.pm: handle_request: \n" . Dumper( \@licenses ) . "\n";
-    print STDERR "# Order.pm: Obj is:\n";
-    print STDERR "# Order.pm: handle_request: \n" . Dumper( \%obj ) . "\n";
 
     $response->{data} = \@licenses;
-    print STDERR "# Order.pm: Final response is:\n";
-    print STDERR "# Order.pm: handle_request: \n" . Dumper( \$response ) . "\n";
 
     return $response;
 }
